386d4f900588795a0e3d566b0c677b4ea844af0f
Rzmvski <victor.razumovski@gmail.com>
2022-12-06 18:06:30 +0300

    Add properties

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,16 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/myDB";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String DRIVER_KEY = "db.driver";
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Class.forName(PropertiesUtil.get(DRIVER_KEY));
+            connection = DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
         } catch (ClassNotFoundException | SQLException e) {
             e.getStackTrace();
         }
